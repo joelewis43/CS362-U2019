6ae30cacd826ff1e4fb10f615b2eaad90444e610
Joseph Lewis <lewisjos@flip1.engr.oregonstate.edu>
2019-08-05 20:43:32 -0700

    copied the FinalProject into proejcts/lewisjos

---
+++ projects/lewisjos/FinalProject/URLValidatorInCorrect/test/UrlValidatorUnitTest/ResultPair.java
@@ -0,0 +1,13 @@
+package UrlValidatorUnitTest;
+
+public class ResultPair {
+
+	public final String item;
+	public final boolean valid;
+	
+	public ResultPair(String item, boolean valid) {
+		this.item = item;
+		this.valid = valid;
+	}
+	
+}
